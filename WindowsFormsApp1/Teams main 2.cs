dbba2ec583841f2a2adcec2e3bda69af8132a7f8
Никита Неклюдов <you@example.com>
2022-03-24 22:26:48 +0300

    1

---
+++ WindowsFormsApp1/Teams main 2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Teams_Main__2_ : Form
+    {
+        public Teams_Main__2_()
+        {
+            InitializeComponent();
+        }
+
+        private void label33_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label39_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label61_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label63_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label64_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label40_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label41_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label60_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label62_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label67_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label56_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label57_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label59_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label65_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void label66_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_Detail();
+            adminForm.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Teams_main();
+            adminForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Visitor_Menu();
+            adminForm.Show();
+        }
+    }
+}
